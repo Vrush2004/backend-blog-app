a2f2a87a8e94a8d5682dcf55d84845c34ff3df4d
Vrush2004 <vrushalichaube@gmail.com>
2024-07-28 11:43:49 +0530

    Authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userControllers.js";
+import { registerUser, loginUser, userProfile } from "../controllers/userControllers.js";
 const router = express.Router();
+import {authGuard} from "../middleware/authMiddleware.js"
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/profile',authGuard, userProfile);
 
 export default router;
