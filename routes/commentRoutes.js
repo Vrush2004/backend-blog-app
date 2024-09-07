2bed2ebbc5ac20b18f0474b801d8175d8e552f6d
Vrush2004 <vrushalichaube@gmail.com>
2024-09-07 12:13:23 +0530

    delete comment functionality added

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
-import {createComment, UpdateComment } from "../controllers/commentControllers.js";
+import {createComment, deleteComment, UpdateComment } from "../controllers/commentControllers.js";
 import {authGuard} from "../middleware/authMiddleware.js"
 
 router.post('/', authGuard, createComment);
-router.put('/:commentId',authGuard, UpdateComment)
+router.route('/:commentId').put(authGuard, UpdateComment).delete(authGuard, deleteComment)
 
 export default router;
